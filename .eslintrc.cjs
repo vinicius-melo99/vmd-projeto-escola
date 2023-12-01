b3a516ee7799275d03723d9abe3d5da8ea55f70a
Vinícius Melo <viniciusmelo317@gmail.com>
2023-12-01 11:22:59 -0300

    chore: configurações de eslint, prettier e editorconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,21 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
+    'eslint-config-airbnb-base',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react-hooks', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 }
