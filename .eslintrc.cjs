ad7fa48b65e5b4019fc8818f8ecb6bb784760f80
Vinícius Melo <viniciusmelo317@gmail.com>
2023-12-01 11:44:18 -0300

    chore: limpeza de arquivos não essenciais

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,5 +22,6 @@ module.exports = {
     'import/prefer-default-export': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'comma-dangle': 0
   },
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import './App.css';
-
 function App() {
-  return (
-    <>
-      <h1>Olá</h1>
-    </>
-  );
+  return <h1>Olá</h1>;
 }
 
 export default App;
