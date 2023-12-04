280438e5a397b0616cfb1cff6a2710250e38f8a5
Vinícius Melo <viniciusmelo317@gmail.com>
2023-12-04 12:20:20 -0300

    feat: adicionado novo header na aplicação com botões de home, sign out e user

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Login from './pages/Login/Login.jsx';
 import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header/Header.jsx';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyles />
     </>
